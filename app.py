4d1ca04a6da3ac275e3c932927a26c7039069532
Hiuston957 <blackeyesblackbear@gmail.com>
2025-01-07 21:14:33 +0100

    add base file

--- app.py
+++ app.py
@@ -1,1 +1,42 @@
-print('Hello from WSEI_flask')
+from flask import Flask, render_template
+
+from datetime import datetime
+
+import pytz
+
+
+
+app = Flask(__name__)
+
+
+
+def get_time_in_timezone(timezone):
+
+    tz = pytz.timezone(timezone)
+
+    return datetime.now(tz).strftime('%Y-%m-%d %H:%M:%S')
+
+
+
+@app.route('/')
+
+def home():
+
+    times = {
+
+        "Polska": get_time_in_timezone("Europe/Warsaw"),
+
+        "USA (Nowy Jork)": get_time_in_timezone("America/New_York"),
+
+        "Chiny (Pekin)": get_time_in_timezone("Asia/Shanghai")
+
+    }
+
+    return render_template('index.html', times=times)
+
+
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', port=5000)
+
